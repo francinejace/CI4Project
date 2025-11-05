aeff735d08c5c31ccc78decc6b359310515c31ee
francinejace <202310726@fit.edu.ph>
2025-11-05 22:58:41 +0800

    Update coffee menu prices to realistic values and add new session file

--- app/Controllers/Customer.php
+++ app/Controllers/Customer.php
@@ -8,10 +8,11 @@ class Customer extends BaseController
     public function index()
     {
         // Minimal coffee menu
+        // Prices updated to realistic values (PHP pesos) - minimum ₱175
         $menu = [
-            ['name' => 'Espresso', 'price' => '2.50'],
-            ['name' => 'Cappuccino', 'price' => '3.00'],
-            ['name' => 'Latte', 'price' => '3.50'],
+            ['name' => 'Espresso', 'price' => 175.00],
+            ['name' => 'Cappuccino', 'price' => 200.00],
+            ['name' => 'Latte', 'price' => 225.00],
         ];
 
         return view('customer/index', ['menu' => $menu]);
